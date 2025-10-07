96ce3692349e8642004030ab6b17f4572619f91d
moonbeam0281 <moonbeam32145@gmail.com>
2025-10-07 13:57:54 +0200

    Created first homework and did first assignment on test-app

---
+++ test-app/src/components/MyFirstComponent.jsx
@@ -0,0 +1,6 @@
+
+export default function MyFirstComponent() {
+    return (
+        <h2 id = "myDiv">React is awesome!</h2>
+    )
+}
---
+++ test-app/src/components/MySecondComponent.jsx
@@ -0,0 +1,12 @@
+import { Component } from "react";
+
+class MyClassComp extends Component{
+
+    render() {
+        return (
+            <h3>Class components are powerful!</h3>
+        )
+    }
+}
+
+export default MyClassComp;
