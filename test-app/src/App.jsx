b357bc057e23011eb730b15ebd8058b42bbdf337
moonbeam0281 <moonbeam32145@gmail.com>
2025-10-08 22:03:23 +0200

    Updating the repo, getting ready for 2nd homework assignment

--- test-app/src/App.jsx
+++ test-app/src/App.jsx
@@ -1,15 +1,23 @@
 import './App.css'
 import MyFirstComponent from './components/MyFirstComponent'
 import MyClassComp from './components/MySecondComponent'
+import UserProfileClass from './components/UserProfileClass'
 
 function App() {
   return (
     <>
-      <MyClassComp />
-      <MyFirstComponent />
-
+      <UserProfileClass
+        name="Luna"
+        age="22"
+        hobby="Night rider" 
+        />
     </>
   )
 }
+
+/*
+<MyClassComp />
+      <MyFirstComponent />
+*/
 
 export default App
---
+++ test-app/src/components/UserProfileClass.jsx
@@ -0,0 +1,20 @@
+import {Component} from "react";
+
+class UserProfileClass extends Component{
+
+
+    render()
+    {
+        const {name, age, hobby} = this.props;
+
+        return(
+            <div>
+                <h2>{name}</h2>
+                <p>Age: {age}</p>
+                <p>Hobby: {hobby}</p>
+            </div>
+        );
+    }
+}
+
+export default UserProfileClass;
