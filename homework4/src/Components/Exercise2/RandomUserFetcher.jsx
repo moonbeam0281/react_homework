529c0d0ed6b294fcafd30840b88ce3a1524dab2a
moonbeam0281 <moonbeam32145@gmail.com>
2025-10-20 17:56:22 +0200

    Added error handling on RandomUserFetcher.jsx and some extra empty object check

--- homework4/src/Components/Exercise2/RandomUserFetcher.jsx
+++ homework4/src/Components/Exercise2/RandomUserFetcher.jsx
@@ -22,36 +22,70 @@ function UserCard({ userData }) {
     )
 }
 
+function ErrorBox({ err }) {
+    if(!err) return null;
+    return (
+        <div>
+            <h1>Error while fetching info!</h1>
+            <p>{err.message}</p>
+        </div>
+    )
+}
+
 export default function RandomUserFetcher() {
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(false);
+    const [error, setErr] = useState(null);
 
     async function getUser() {
+        setLoading(true);
+        setErr(null);
+        setUser(null);
+        const id = randomId();
         try {
-            setLoading(true);
-            const id = randomId();
             const res = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
-            if (!res.ok) console.log("Error with the request");
+            if (!res.ok) {
+                console.log("Error with the request");
+                throw new Error(`Bad request (${res.status})\n${res.statusText}`);
+            }
             const data = await res.json();
+
+            if(!data) throw new Error(`Bad data. Could not load info.\n${res.statusText}`);
+
             setUser(data);
         }
-        catch (e) {
-            console.log("Error while running the getUserFunction")
+        catch (error) {
+            setErr(error);
+            console.log(`Internal error while running the getUserFunction\n${error}`);
         }
         finally {
-            console.log(user);
+            //console.log(user);
+            setLoading(false);
+            /*
             setTimeout(() => {
-                setLoading(false);
-            }, 500);
+                this was just for visually looking nice
+            }, 500);*/
         }
     }
 
     return (
         <div className="userBox">
-            <button onClick={getUser}>
-                {loading? "Fetching user data..." : "Get user data"}
+            <button onClick={getUser} disabled = {loading}>
+                {loading ? "Fetching user data..." : "Get user data"}
             </button>
+
             <UserCard userData={user} />
+            {error && <ErrorBox err={error} />}
         </div>
     );
 }
+
+//{<button onClick={errorTest}>Error box test</button>}
+// Button for testing error message
+
+/*
+
+    function errorTest() {
+        setErr("Big error! something bad hapened!");
+    }
+*/
